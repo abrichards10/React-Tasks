8a83a7ae0d9b9bdfad4f75fa5df32187d566e4b3
abrichards <angelarichards261@gmail.com>
2023-09-06 00:54:59 -0700

    added task calendar synced funcitonality

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -5,79 +5,11 @@ import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from "@fullcalendar/interaction"
 import { v4 as uuid } from "uuid";
 
-// import Toggle from "react-toggle";
-// import { useMediaQuery } from "react-responsive";
-// import { useEffect, useMemo } from "react";
-// import createPersistedState from "use-persisted-state";
-// const useColorSchemeState = createPersistedState("colorScheme");
-
 const Projects = () => {
     const [isSidebarOpen] = useState(false);
-    // const { isDark, setIsDark } = useColorScheme();
-    //
-    // function useColorScheme() {
-    //     const systemPrefersDark = useMediaQuery(
-    //         {
-    //             query: "(prefers-color-scheme: dark)",
-    //         },
-    //         undefined
-    //     );
-    //
-    //     const [isDark, setIsDark] = useColorSchemeState();
-    //     const value = useMemo(
-    //         () => (isDark === undefined ? !!systemPrefersDark : isDark),
-    //         [isDark, systemPrefersDark]
-    //     );
-    //
-    //     useEffect(() => {
-    //         if (value) {
-    //             document.body.classList.add("dark");
-    //         } else {
-    //             document.body.classList.remove("dark");
-    //         }
-    //     }, [value]);
-    //
-    //     return {
-    //         isDark: value,
-    //         setIsDark,
-    //     };
-    // }
-
-
-    // const submitButton = document.getElementById('submitButton');
-    // const infoList = document.getElementById('infoList');
-    // const userInfoForm = document.getElementById('userInfoForm');
-    // const calendarEl = document.getElementById('calendar');
-    // const customPrompt = document.getElementById('customPrompt');
-    // const cancelButton = document.getElementById('cancelButton');
-    //
-    // const nameInput = document.getElementById('name');
-    // const eventNameInput = document.getElementById('eventNameInput');
-    // const customEditPrompt = document.getElementById('customEditPrompt');
-    //
-    // const editEventNameInput = document.getElementById('editEventNameInput');
-    // const editCancelButton = document.getElementById('editCancelButton');
-    // const editConfirmButton = document.getElementById('editConfirmButton');
-    // const confirmButton = document.getElementById('confirmButton');
-    // let name = "";
-    //
-    // let lastClickedDate = null;
-    // let dateList = [];
-    // let currentID = null;
-    // let localID = null;
-
-    // const calendar = new FullCalendar.Calendar(calendarEl, {
-    //     initialView: 'dayGridMonth',
-    //     eventMouseEnter: handleEventMouseEnter,
-    //     eventMouseLeave: handleEventMouseLeave,
-    //     headerToolbar: {
-    //         left: 'prev, next today',
-    //         center: 'title',
-    //         right: 'dayGridMonth,timeGridWeek,timeGridDay',
-    //     },
-    //     dateClick: dateClickFunction,
-    //     eventClick: eventClickFunction,
-    // });
+    const [events, setEvents] = useState([]);
+    const [task, setTask] = useState(""); // State to store the task input
+    const [taskList, setTaskList] = useState([]);
 
     // Function to open the sidebar
     const openNav = () => {
@@ -91,191 +23,17 @@ const Projects = () => {
         document.getElementById("main").style.marginLeft = "10px";
     };
 
-
-    // function eventClickFunction(info) {
-    //     currentID = info.id;
-    //     customEditPrompt.style.display = 'block';
-    //     lastClickedDate = info.date; // Store the last clicked date
-    // }
-    //
-    // function dateClickFunction(info) {
-    //     eventNameInput.value = '';
-    //     customPrompt.style.display = 'block';
-    //     lastClickedDate = info.date; // Store the last clicked date
-    // }
-
-    // function handleFormSubmit() {
-    //     const currentDate = new Date();
-    //     let formattedDate = currentDate.toISOString();
-    //
-    //     if (lastClickedDate == null) {
-    //         formattedDate = currentDate.toISOString();
-    //     } else {
-    //         formattedDate = lastClickedDate.toISOString();
-    //     }
-    //
-    //     dateList.push(currentID);
-    //     console.log(dateList);
-    //
-    //     const entry = document.createElement('li');
-    //     entry.innerHTML = `
-    //             <button class="deleteButton">x</button>
-    //             <span contenteditable="true">${name}</span>
-    //             <span class="taskDate" data-id="${formattedDate}"></span><br>
-    //         `;
-    //
-    //     const event = {
-    //         title: name,
-    //         start: formattedDate,
-    //         id: formattedDate,
-    //     };
-    //
-    //     const existingEvent = calendar.getEventById(formattedDate);
-    //     if (existingEvent) {
-    //         existingEvent.setProp('title', name);
-    //         entry.querySelector('.taskDate').textContent = formattedDate.toLocaleDateString();
-    //     } else {
-    //         calendar.addEvent(event);
-    //     }
-    //
-    //     infoList.appendChild(entry);
-    //     userInfoForm.reset();
-    //
-    //     const deleteButton = entry.querySelector('.deleteButton');
-    //     deleteButton.addEventListener('click', () => {
-    //         const eventToRemove = calendar.getEventById(formattedDate);
-    //         if (eventToRemove) {
-    //             eventToRemove.remove();
-    //         }
-    //
-    //         infoList.removeChild(entry);
-    //     });
-    //
-    //     customPrompt.style.display = 'none';
-    // }
-
-    /** PRESSING EDIT ENTER FUNCTIONS **/
-    // editEventNameInput.addEventListener('keydown', (event) => {
-    //     if (event.key === 'Enter') {
-    //
-    //         name = editEventNameInput.value;
-    //
-    //         for (let i = 0; i < dateList.length; i++) {
-    //             if (currentID === dateList[i]) {
-    //                 localID = currentID;
-    //                 break;
-    //             }
-    //         }
-    //
-    //         const existingEvent = calendar.getEventById(localID);
-    //         existingEvent.setProp('title', name);
-    //
-    //         document.getElementById('infoList').innerHTML = `
-    //                 <button class="deleteButton">x</button>
-    //                 <span contenteditable="true">${name}</span>
-    //                 <span class="taskDate" data-id="${localID}"></span><br>
-    //             `;
-    //
-    //         customEditPrompt.style.display = 'none';
-    //     }
-    // });
-
-    // editCancelButton.addEventListener('click', () => {
-    //     customEditPrompt.style.display = 'none';
-    // });
-
-    // editConfirmButton.addEventListener('click', () => {
-    //     name = editEventNameInput.value;
-    //
-    //     for (let i = 0; i < dateList.length; i++) {
-    //         if (currentID === dateList[i]) {
-    //             localID = currentID;
-    //             const existingEvent = calendar.getEventById(localID);
-    //             existingEvent.setProp('title', name);
-    //
-    //             document.getElementById('infoList').innerHTML = `
-    //                     <button class="deleteButton">x</button>
-    //                     <span contenteditable="true">${name}</span>
-    //                     <span class="taskDate" data-id="${currentID}"></span><br>
-    //                 `;
-    //         }
-    //         customEditPrompt.style.display = 'none';
-    //     }
-    // });
-
-
-    /** PRESSING ENTER FUNCTIONS **/
-    // nameInput.addEventListener('keydown', (event) => {
-    //     name = nameInput.value;
-    //     if (event.key === 'Enter') {
-    //         event.preventDefault();
-    //         handleFormSubmit();
-    //     }
-    // });
-    //
-    // eventNameInput.addEventListener('keydown', (event) => {
-    //     name = eventNameInput.value;
-    //     if (event.key === 'Enter') {
-    //         event.preventDefault();
-    //         handleFormSubmit();
-    //     }
-    // });
-
-
-    /** BUTTON FUNCTIONS **/
-    // confirmButton.addEventListener('click', () => {
-    //     name = eventNameInput.value
-    //     handleFormSubmit();
-    // });
-    //
-    // cancelButton.addEventListener('click', () => {
-    //     customPrompt.style.display = 'none';
-    // });
-    //
-    // submitButton.addEventListener('click', () => {
-    //     name = nameInput.value
-    //     handleFormSubmit();
-    // });
-
-
-    // when user hits enter or button when creating an event
-    // it creates an id for that event and adds it to a list
-
-    // when the user later clicks an event to edit it
-    // get the value of the event clicked
-    // after they hit enter or button --> go through list
-    // if currentID = list id then that's the new id
-    //
-
-
     /** HOVER FUNCTIONS **/
-    // function handleEventMouseEnter(info) {
-    //     const eventId = info.event.id;
-    //     const correspondingTask = document.querySelector(`.taskDate[data-id="${eventId}"]`);
-    //     if (correspondingTask) {
-    //         correspondingTask.parentNode.classList.add('highlighted');
-    //     }
-    // }
-    // function handleEventMouseLeave(info) {
-    //     const eventId = info.event.id;
-    //     const correspondingTask = document.querySelector(`.taskDate[data-id="${eventId}"]`);
-    //     if (correspondingTask) {
-    //         correspondingTask.parentNode.classList.remove('highlighted');
-    //     }
-    // }
+    function handleEventMouseEnter(info) {
 
-    /*** DARK MODE ***/
+    }
+    function handleEventMouseLeave(info) {
 
-    /** CREATE CALENDAR **/
-    // document.addEventListener('DOMContentLoaded', function() {
-    //     calendar.render();
-    // });
-
-    const [events, setEvents] = useState([]);
+    }
 
     const handleSelect = (info) => {
         const { start, end } = info;
-        const eventNamePrompt = prompt("Enter, event name");
+        const eventNamePrompt = prompt("Enter event name");
         if (eventNamePrompt) {
             setEvents([
                 ...events,
@@ -286,8 +44,32 @@ const Projects = () => {
                     id: uuid(),
                 },
             ]);
+            setTaskList([...taskList, eventNamePrompt]);
         }
     };
+    const EventItem = ({ info }) => {
+        const { event } = info;
+        return (
+            <div>
+                <p>{event.title}</p>
+            </div>
+        );
+    };
+
+    const handleTaskInputChange = (event) => {
+        setTask(event.target.value);
+    };
+
+    const handleTaskSubmit = () => {
+        if (task) {
+            // Add the task to the task list
+            setTaskList([...taskList, task]);
+
+            // Clear the task input
+            setTask("");
+        }
+    };
+
 
     return (
         <div>
@@ -309,12 +91,10 @@ const Projects = () => {
                 <Link to="/settings">Settings</Link>
             </div>
         </div>
+
         <div id="main">
             <button className="open-button" onClick={openNav}>&#9776;</button>
-            <h2>Projects</h2>
-            <p>Content...</p>
-        </div>
-        <div id="main">
+
             {/*<Toggle*/}
             {/*    checked={isDark}*/}
             {/*    onChange={({ target }) => setIsDark(target.checked)}*/}
@@ -327,14 +107,26 @@ const Projects = () => {
 
                     <form id="userInfoForm">
                         <label htmlFor="name">Enter Task:</label>
-                        <input type="text" id="name" name="name"/>
-                            <button type="button" id="submitButton">Submit</button>
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            value={task}
+                            onChange={handleTaskInputChange}
+                        />
+                        <button type="button" onClick={handleTaskSubmit}>
+                            Submit
+                        </button>
                     </form>
 
-                    {/*<div id="displayInfo">*/}
-                    {/*    <h2>Tasks:</h2>*/}
-                    {/*    <ul id="infoList"></ul>*/}
-                    {/*</div>*/}
+                    <div id="displayInfo">
+                        <h2>Tasks:</h2>
+                        <ul id="infoList">
+                            {taskList.map((task, index) => (
+                                <li key={index}>{task}</li>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
 
                 <div class="second-column">
@@ -346,6 +138,9 @@ const Projects = () => {
                             start: "today prev next",
                             end: "dayGridMonth dayGridWeek dayGridDay",
                         }}
+                        eventContent={(info) => <EventItem info={info} />}
+                        eventMouseEnter={handleEventMouseEnter}
+                        eventMouseLeave={handleEventMouseLeave}
                         events={events}
                         select={handleSelect}
                         plugins={[dayGridPlugin, interactionPlugin]}
